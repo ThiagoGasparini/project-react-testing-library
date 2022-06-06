192b8a61c226992fd852767747e01227d56bf2a9
Thiago Gasparini <thiago_engprod@outlook.com>
2022-06-06 15:30:09 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+beforeEach(() => render(<NotFound />));
+
+test('página contém um heading h2 com o texto Page requested not found', () => {
+  const notFound = screen.getByRole('heading', {
+    name: /Page requested not found/i,
+  });
+  expect(notFound).toBeInTheDocument();
+});
+
+test(
+  'página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  () => {
+    const notFoundImage = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(notFoundImage).toBeInTheDocument();
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  },
+);

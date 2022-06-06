a9c022b0c1e594e88779de70cf46e6528d9d23e9
Thiago Gasparini <thiago_engprod@outlook.com>
2022-06-06 15:23:31 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('exibição do texto "No favorite pokemon found"', () => {
+  renderWithRouter(<App />);
+
+  const favoritesLink = screen.getByRole('link', { name: /Favorite pokémons/i });
+  userEvent.click(favoritesLink);
+
+  const textFavorites = screen.getByText('No favorite pokemon found');
+  expect(textFavorites).toBeInTheDocument();
+});
+
+test('exibição de todos os cards de pokémons favoritados.', async () => {
+  renderWithRouter(<App />);
+
+  const moreDetailsLink = screen.getByRole('link', { name: /More details/i });
+  userEvent.click(moreDetailsLink);
+
+  const checkbox = screen.getByRole('checkbox');
+  userEvent.click(checkbox);
+
+  // https://developer.mozilla.org/pt-BR/docs/Web/API/Node/textContent
+  const pokemonName = screen.getByTestId('pokemon-name').textContent;
+
+  const favoritesLink = screen.getByRole('link', { name: /Favorite pokémons/i });
+  userEvent.click(favoritesLink);
+
+  const textPokemonName = screen.getByText(pokemonName);
+  expect(textPokemonName).toBeInTheDocument();
+});
